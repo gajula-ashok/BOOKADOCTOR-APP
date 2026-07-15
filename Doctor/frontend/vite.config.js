7749a172ee9152fc4fc800946d783107f2ac8a89
GAJULA ASHOK <chiruashok12@gmail.com>
2026-07-15 13:28:03 +0530

    updated frontend config

--- Doctor/frontend/vite.config.js
+++ Doctor/frontend/vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://bookadoctor-app-5.onrender.com',
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://bookadoctor-app-5.onrender.com',
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-});
+        secure: false,
+      },
+    },
+  },
+})
